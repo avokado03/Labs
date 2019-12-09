df23481a7669d47fa65a7e3013366fa9ff3027a1
avokado <alotofcode95@gmail.com>
2019-12-09 23:32:02 +0400

    final

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,12 +1,13 @@
 #include "pch.h"
 #include "stdio.h"
 #include "conio.h"
-#include <typeinfo>
-#include <exception>
 #include <iostream>
-#include <locale>
+#include <Windows.h>
 
 
+// объ€вл€ем функции
+// можно их просто тут определить, не объ€вл€€
+// https://metanit.com/cpp/tutorial/3.1.php
 const char* Conditions(int a, int X, int Y);
 void Cycles(int X, char symbol);
 int Arrays(const char* source, const char* charSet);
@@ -14,34 +15,51 @@ int Arrays(const char* source, const char* charSet);
 
 int main()
 {
+	//устанавливаем кодировку консоли
+	SetConsoleCP(1251);
+	//устанавливаем €зык
 	setlocale(LC_ALL, "Russian");
-	//setlocale(LC_ALL, "ru_UA.UTF-8");
 
-#pragma region U5
+// pragma region: https://docs.microsoft.com/ru-ru/cpp/preprocessor/region-endregion?view=vs-2019
+// писать вам не надо, мне прост так удобнее
+// чтобы запустить вызовы какой-то функции, раскомментируйте ее регион
+
+//услови€
+#pragma region Conditions
 	//printf("%s\n", Conditions(1, 2, 4));
 	//printf("%s\n", Conditions(2, 0, 5));
 	//printf("%s\n", Conditions(-1, -2, 2));
 #pragma endregion
 
-#pragma region TS5
+//циклы
+#pragma region Cycles
 	//Cycles(2, '*');
 	//Cycles(8, '8');
 	//Cycles(0, 'b');
 #pragma endregion
 
-#pragma region M5
+//массивы
+#pragma region Arrays
 	printf("%d\n", Arrays("Unique string", "in")); //4
-	printf("%d\n", Arrays("я совсем забыла, как писать на C++", "a+")); //7
+	printf("%d\n", Arrays("я совсем забыла, как писать на C++", "а+")); //7
 	printf("%d\n", Arrays(" ака€-то строка", "f")); //0
 #pragma endregion
 
+	// не дает закрытьс€ консольке, сидит в conio.h
+	// похоже на Console.ReadKey() в шарпе
+	// http://nullflow.blogspot.com/2011/05/c-getch.html
 	_getch();
 }
 
+
+/* определ€ем функции
+ про const, типы данных и указатели
+ ищите в MSDN, мне лень писать*/
 const char* Conditions(int a, int X, int Y)
 {
 	
 	const char* result;
+	//сразу присваиваем "нет", чтобы от else избавитьс€
 	result = "Ќет";
 	if (a >= X && a <= Y)
 	{
@@ -52,6 +70,7 @@ const char* Conditions(int a, int X, int Y)
 
 void Cycles(int X, char symbol)
 {	
+	//ну тут €вно число положительное нужно
 	if (X > 0) 
 	{
 		for (int i = 0; i < X; i++)
@@ -68,12 +87,18 @@ void Cycles(int X, char symbol)
 
 int Arrays(const char* source, const char* charSet)
 {
+	
 	int result = 0;
+	// strlen http://cppstudio.com/post/664/
+	// сравниваем все символы из 1го массива 
 	for (int i = 0; i < strlen(source); i++)
 	{
+		//с каждым символом 2го массива
 		for (int j = 0; j < strlen(charSet); j++)
 		{
 			if (source[i] == charSet[j])
+				// увеличиваем счетчик, если символы совпали, на 1
+				// постфиксный инкремент http://cppstudio.com/post/282/
 				result++;
 		}
 	}
